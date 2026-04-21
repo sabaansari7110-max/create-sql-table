a655a5186f5168be6b5826f9d32c9aaa7eae24f1
Saba Ansari <sabaansari7110@gmail.com>
2026-04-21 12:58:42 +0530

    Add files via upload

---
+++ queries/insert_values.sql
@@ -0,0 +1,32 @@
+USE startersql;
+INSERT INTO companymembers ( name, email, gender, DOB)  VALUES 
+( 'Jhonn' ,'jam00@gmail.com', 'MALE', '2002-11-07' ),
+( 'elliss' ,'eliiis00@gmail.com', 'MALE', '2002-11-07' ),
+( 'venlin' ,'venio00@gmail.com', 'MALE', '2002-11-07' ),
+( 'deriy' ,'deriy00@gmail.com', 'MALE', '2000-12-27' ),
+( 'clark' ,'clark0@gmail.com', 'MALE', '2004-06-27' ),
+( 'taylor' ,'taylor0@gmail.com', 'FEMALE', '1997-01-31' ),
+( 'walker' ,'walker@gmail.com', 'MALE', '2004-12-27' ),
+( 'ravi' ,'ravi0@gmail.com', 'MALE', '2001-11-19' ),
+( 'chloe' ,'chloe0@gmail.com', 'OTHER', '1994-04-07' ),
+( 'nora' ,'nora00@gmail.com', 'MALE', '2003-09-23' ),
+( 'jack' ,'jack0@gmail.com', 'MALE', '1999-11-07' ),
+( 'ava' ,'ava@gmail.com', 'FEMALE', '1992-04-28' ),
+( 'james' ,'james0@gmail.com', 'OTHER', '2002-12-27' ),
+( 'zara' ,'zara@gmail.com', 'FEMALE', '2000-11-07' ),
+( 'aamir' ,'aamir200@gmail.com', 'MALE', '2002-08-02' ),
+( 'luca' ,'lucas0@gmail.com', 'MALE', '2002-01-17' ),
+( 'mia' ,'mai@gmail.com', 'FEMALE', '2001-06-27' ),
+( 'arjun' ,'arjun00@gmail.com', 'MALE', '1995-11-3' ),
+( 'sofia' ,'sofia0@gmail.com', 'FEMALE', '2000-06-27' ),
+( 'ethan' ,'ethan00@gmail.com', 'MALE', '1999-11-07' ),
+( 'isabella' ,'ishabella0@gmail.com', 'FEMALE', '2002-05-15' ),
+( 'noah' ,'noah00@gmail.com', 'OTHER', '1998-09-07' ),
+( 'olivia' ,'olivia0@gmail.com', 'FEMALE', '1992-01-17' ),
+( 'chen' ,'chen00@gmail.com', 'MALE', '1999-11-07' ),
+( 'liam' , 'liam@gmail.com', 'MALE', '1999-11-10' ),
+( 'fatima' ,'fatimakhan0@gmail.com', 'FEMALE', '2001-06-14' ),
+( 'Carlos' ,'carloss00@gmail.com', 'MALE', '1992-09-16' ),
+( 'emily' ,'emily6789@gmail.com', 'FEMALE', '1990-12-07' );
+
+SELECT *FROM companymembers;
